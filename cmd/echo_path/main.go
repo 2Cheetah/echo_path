30805c793c1933a90f238cd03dd2cf34b1d577ea
Denis Safiullin <fckninja@gmail.com>
2024-04-07 15:14:14 +0000

    code refactoring and making the app os agnostic

--- cmd/echo_path/main.go
+++ cmd/echo_path/main.go
@@ -4,24 +4,32 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"path/filepath"
 	"sort"
 	"strings"
 )
 
 func main() {
-	pathString := os.Getenv("PATH")
-	listOfPaths := strings.Split(pathString, ":")
-
+	// Parse command-line flags
 	sortFlag := flag.Bool("s", false, "sort the variables")
 	sortFlagAlias := flag.Bool("sort", false, "sort the variables")
-
 	flag.Parse()
 
+	// Get the PATH environment variable
+	pathString := os.Getenv("PATH")
+	listOfPaths := strings.Split(pathString, string(filepath.ListSeparator))
+
+	// Sort the paths if the sort flag is provided
 	if *sortFlag || *sortFlagAlias {
 		sort.Strings(listOfPaths)
 	}
 
-	for _, s := range listOfPaths {
-		fmt.Println(s)
+	// Print the paths
+	printPaths(listOfPaths)
+}
+
+func printPaths(paths []string) {
+	for _, p := range paths {
+		fmt.Println(p)
 	}
 }
